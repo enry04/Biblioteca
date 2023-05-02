fc7041436ee62eb3fc5fc6ee0fb972ad5f770ef0
enry04 <enrico04v@gmail.com>
2023-05-02 08:48:04 +0200

    init header

---
+++ src/common/php/header.php
@@ -0,0 +1,82 @@
+<header>
+    <div class="logo-container">
+        <div class="logo"></div>
+        <h3 class="extra-bold brown">Biblioteca <br>Volta</h3>
+    </div>
+    <!-- <div class="voices-container">
+        <?php
+        if ($page == "main") {
+        ?>
+            <div class="active-container">
+                <h3>
+                    <a class="white extra-bold active-page" href="../main-page/main.php">Home</a>
+                </h3>
+            </div>
+            <h3>
+                <a class="brown extra-bold" href="">Prenota</a>
+            </h3>
+            <h3>
+                <a class="brown extra-bold" href="">Le biblioteche</a>
+            </h3>
+            <h3>
+                <a class="brown extra-bold" href="">Accedi</a>
+            </h3>
+        <?php
+        } else if ($page == "prenote") {
+        ?>
+            <h3>
+                <a class="extra-bold brown" href="../main-page/main.php">Home</a>
+            </h3>
+            <div class="active-container">
+                <h3>
+                    <a class="white extra-bold active-page" href="">Prenota</a>
+                </h3>
+            </div>
+            <h3>
+                <a class="brown extra-bold" href="">Le biblioteche</a>
+            </h3>
+            <h3>
+                <a class="brown extra-bold" href="">Accedi</a>
+            </h3>
+        <?php
+        } else if ($page == "libraries") {
+        ?>
+            <h3>
+                <a class="brown extra-bold" href="../main-page/main.php">Home</a>
+            </h3>
+            <h3>
+                <a class="brown extra-bold" href="">Prenota</a>
+            </h3>
+            <div class="active-container">
+                <h3>
+                    <a class="white extra-bold active-page" href="">Le biblioteche</a>
+                </h3>
+            </div>
+            <h3>
+                <a class="brown extra-bold" href="">Accedi</a>
+            </h3>
+        <?php
+        } else if ($page == "login") {
+        ?>
+            <h3>
+                <a class="brown extra-bold" href="../main-page/main.php">Home</a>
+            </h3>
+            <h3>
+                <a class="brown extra-bold" href="">Prenota</a>
+            </h3>
+            <h3>
+                <a class="brown extra-bold" href="">Le biblioteche</a>
+            </h3>
+            <div class="active-container">
+                <h3>
+                    <a class="white extra-bold active-page" href="">Accedi</a>
+                </h3>
+            </div>
+        <?php
+        }
+        ?>
+    </div> -->
+    <div class="voices-container">
+        
+    </div>
+</header>
--- src/main-page/main.php
+++ src/main-page/main.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca</title>
     <link rel="stylesheet" href="../common/css/page-style.css">
+    <link rel="stylesheet" href="../common/css/header-style.css">
 </head>
+
 <body>
-    
+    <?php
+    $page = "prenote";
+    $active = 'class="active-page"';
+    require_once("../common/php/header.php");
+    ?>
+
 </body>
+
 </html>
