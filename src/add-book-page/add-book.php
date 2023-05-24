31b8ff4e9d7f73237019244de703ac3aa248021c
enrico visentin <enrico04v@gmail.com>
2023-05-24 21:26:25 +0200

    ciao

---
+++ src/add-book-page/add-book.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca</title>
+    <link rel="stylesheet" href="../common/css/page-style.css">
+    <link rel="stylesheet" href="../common/css/header-style.css">
+    <link rel="stylesheet" href="../common/css/form-style.css">
+    <link rel="stylesheet" href="./css/add-book-style.css">
+    <link rel="icon" href="../common/images/icons/volta-icon.png">
+</head>
+
+<body>
+    <?php
+    require_once("../common/php/token-manager.php");
+    TokenManager::setPage("add-book");
+    require_once("../common/php/header.php");
+    ?>
+</body>
+
+</html>
